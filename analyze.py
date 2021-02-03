f7a19647788feb017db4678ccba6c58cb7041a38
leukipp <leukipp@outlook.com>
2021-02-03 01:02:36 +0100

    load data

--- analyze.py
+++ analyze.py
@@ -14,13 +14,18 @@ import streamlit as st
 # %% SETTINGS
 warnings.filterwarnings('ignore')
 pd.set_option('display.max_rows', 20)
-st.set_page_config(layout='wide', initial_sidebar_state='expanded', page_title='Reddit-Yolo')
 
 
-# %% GENREAL INFORMATION
-st.title('Reddit-Yolo')
+# %% CONFIG
 with open(os.path.join('config', 'config.json')) as f:
     config = json.load(f)
+title = f'Reddit - r/{config["subreddit"]}'
+st.set_page_config(layout='wide', initial_sidebar_state='expanded', page_title=title)
+st.title(title)
+
+
+# %% INTRO
+st.markdown('TODO')
 
 
 # %% LOAD DATA
@@ -28,19 +33,20 @@ df = pd.read_hdf(os.path.join('data', config['subreddit'], config['reddit']['dat
 
 
 # %% VISUALIZE DATA
+st.title('Data')
+
 dt_keys = sorted(df.dtypes.unique().astype(str))
 dt = st.multiselect('Types', dt_keys, default=dt_keys)
 
 if not dt:
     st.warning('Select datatypes to analyze.')
 else:
-    st.write('Data')
-    st.dataframe(df.select_dtypes(include=dt).head(20))
-
-    st.write('Types')
     st.dataframe(df.select_dtypes(include=dt).dtypes, height=600)
 
+    st.write('Data')
+    st.dataframe(df.select_dtypes(include=dt).tail(20), height=600)
 
-# %% VISUALIZE TODO
+
+# %% ANALYZE TODO
 st.title('TODO')
 st.sidebar.title('TODO')
