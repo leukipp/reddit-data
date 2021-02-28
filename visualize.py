9015cbe2c6fbb7485b601ac6df710e9ce884bf98
leukipp <leukipp@outlook.com>
2021-02-28 21:56:07 +0100

    analyze ticker symbols

--- visualize.py
+++ visualize.py
@@ -1,8 +1,8 @@
 # %% IMPORTS
 import os
+import re
 import json
 import tempfile
-import warnings
 
 import glob as gb
 import numpy as np
@@ -18,15 +18,20 @@ import plotly.graph_objs as go
 from wordcloud import WordCloud
 from collections import Counter
 from common.kaggle import Kaggle
-from nltk.corpus import stopwords
 
 
 # %% INITIALIZATION
-nl.download(['stopwords'])
 pd.options.plotting.backend = 'plotly'
 
 
 # %% FUNCTIONS
+@st.cache(ttl=60*60)
+def download_nltk(data):
+    download_dir = tempfile.mkdtemp()
+    nl.download(data, download_dir=download_dir)
+    return download_dir
+
+
 @st.cache(ttl=60*60)
 def download_dataset(dataset):
     return Kaggle().download(dataset, local=False)
@@ -56,11 +61,12 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded', page_title=t
 st.sidebar.title('Data')
 
 st.sidebar.header('Dataset')
-subreddit = st.sidebar.selectbox('Subreddit', subreddits, index=11)
+subreddit = st.sidebar.selectbox('Subreddit', subreddits, index=subreddits.index('wallstreetbets'))
 
-# download dataset
+nl.data.path.append(download_nltk(['stopwords']))
 df = read_dataset(download_dataset(dataset), file_name=os.path.join(subreddit, f'submissions_reddit.h5'))
 
+# -- FILTER --
 st.sidebar.header('Filter')
 
 # filter created
@@ -75,11 +81,20 @@ exclude = st.sidebar.multiselect('Exclude', flags, default=['removed', 'deleted'
 for e in exclude:
     df_filtered = df_filtered[df_filtered[e] == 0]
 
+if not df_filtered.shape[0]:
+    st.warning('No data available!')
+    st.stop()
+
+# -- SAMPLE --
 st.sidebar.header('Sample')
 
 # sample size
 sample = st.sidebar.slider('Size', value=min(10000, df_filtered.shape[0]), min_value=1, max_value=df_filtered.shape[0])
 df_sampled = df_filtered.sample(frac=1, random_state=42).head(sample)
+
+if not df_sampled.shape[0]:
+    st.warning('No data available!')
+    st.stop()
 
 
 # %% INTRO
@@ -95,7 +110,7 @@ dt_keys = sorted(df.dtypes.unique().astype(str))
 dt = st.multiselect('Types', dt_keys, default=dt_keys)
 
 if not dt:
-    st.warning('Select datatypes to analyze.')
+    st.warning('Select datatypes to analyze!')
 else:
     search = st.text_input('Search', '')
     if search.strip():
@@ -137,9 +152,8 @@ st.altair_chart(chart, use_container_width=True)
 # %% ANALYZE WORDS
 st.title('Words')
 
-text_title = df_sampled['title'].to_string(header=False, index=False)
-text_self = df_sampled['selftext'].to_string(header=False, index=False)
-text_full = f'{text_title} {text_self}'
+topwords = 50
+stopwords = nl.corpus.stopwords.words('english')
 
 args = {
     'background_color': 'white',
@@ -151,11 +165,10 @@ args = {
     'scale': 1.5
 }
 
-topwords = 50
-stopwords = nl.corpus.stopwords.words('english')
 wordcloud = WordCloud(stopwords=stopwords, **args)
+text = re.sub(r'http\S+', '', ''.join(df_sampled['title'].to_list() + df_sampled['selftext'].to_list()))
 
-frequencies = wordcloud.process_text(text_full)
+frequencies = wordcloud.process_text(text)
 wordcloud.generate_from_frequencies(frequencies)
 words = np.array(sorted(frequencies.items(), key=lambda x: x[1], reverse=True))
 
@@ -177,25 +190,30 @@ col_right.image(image, use_container_width=True)
 # %% ANALYZE STOCKS
 st.title('Stocks')
 
-start, end = created[0].strftime('%Y-%m-%d'), created[1].strftime('%Y-%m-%d')
+tickers = open(os.path.join('data', 'tickers.csv')).read().splitlines()
+tickers = words[words['word'].apply(lambda x: any(y for y in tickers if y == x))].head(10).to_dict('records')
 
-stocks = ['GME', 'AMC', 'NOK']
-stock = st.selectbox('Stock', stocks, index=0)
-prices = yf.download(stocks, start=start, end=end, interval='1d', prepost=True)
+if len(tickers):
+    ticker = st.selectbox('Ticker', tickers, format_func=lambda x: f'{x["word"]} (#{x["count"]})', index=0)['word']
 
-args = {
-    'open': prices['Open'][stock],
-    'low': prices['Low'][stock],
-    'high': prices['High'][stock],
-    'close': prices['Close'][stock]
-}
+    start, end = created[0].strftime('%Y-%m-%d'), created[1].strftime('%Y-%m-%d')
+    prices = yf.download(ticker, start=start, end=end, interval='1d', prepost=True)
 
-candlestick = go.Figure(data=[go.Candlestick(x=[x.strftime('%Y-%m-%d %H:%M') for x in prices.index], **args)])
-candlestick.update_layout(
-    height=500,
-    yaxis_title=stock,
-    margin=dict(t=0, b=0, r=0, l=0),
-    annotations=[dict(x=start, y=0.05, xref='x', yref='paper', showarrow=False, xanchor='left', text=' ')]
-)
+    args = {
+        'open': prices['Open'],
+        'low': prices['Low'],
+        'high': prices['High'],
+        'close': prices['Close']
+    }
 
-st.plotly_chart(candlestick, use_container_width=True)
+    candlestick = go.Figure(data=[go.Candlestick(x=[x.strftime('%Y-%m-%d %H:%M') for x in prices.index], **args)])
+    candlestick.update_layout(
+        height=500,
+        yaxis_title=ticker,
+        margin=dict(t=0, b=0, r=0, l=0),
+        annotations=[dict(x=start, y=0.05, xref='x', yref='paper', showarrow=False, xanchor='left', text=' ')]
+    )
+
+    st.plotly_chart(candlestick, use_container_width=True)
+else:
+    st.warning('No ticker symbols found!')
