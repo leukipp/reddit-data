8ef25e5b1747eafa14de191b6ae9b8384dc5af9b
leukipp <leukipp@outlook.com>
2021-03-19 21:57:20 +0100

    run script

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 eval "$(conda shell.bash hook)"
 conda activate reddit
 
-export "$(xargs < .env)"
-rm -f .disabled
+source .env
+export $(grep --regexp ^[A-Z] .env | cut -d= -f1)
 
+rm -f .disabled
 nohup python data.py --publish 7200 > /var/log/reddit.log 2>&1 &
