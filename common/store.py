e08d48fce4218ae25de3a14db7283248cd747422
leukipp <leukipp@outlook.com>
2022-03-31 20:48:40 +0200

    check store exists

--- common/store.py
+++ common/store.py
@@ -1,5 +1,6 @@
 import os
 
+import glob as gb
 import pandas as pd
 import pystore as db
 
@@ -19,8 +20,12 @@ class Store(object):
         self.collection = self.datastore.collection(self.name)
         self.path = os.path.join(self.datastore.datastore, self.collection.collection)
 
+    def exists_meta(self, name):
+        path = os.path.join(self.path, name)
+        return any(gb.glob(os.path.join(path, '*.json')))
+
     def read_meta(self, name):
-        if name in self.collection.list_items():
+        if self.exists_meta(name):
             return db.utils.read_metadata(os.path.join(self.path, name))
         return {}
 
@@ -29,13 +34,17 @@ class Store(object):
         os.makedirs(path, exist_ok=True)
         db.utils.write_metadata(path, metadata=kwargs)
 
+    def exists_data(self, name):
+        path = os.path.join(self.path, name)
+        return any(gb.glob(os.path.join(path, '*.parquet')))
+
     def read_data(self, name):
-        if name in self.collection.list_items():
+        if self.exists_data(name):
             return self.collection.item(name).to_pandas(parse_dates=False)
         return pd.DataFrame()
 
     def write_data(self, name, data, overwrite, **kwargs):
-        if name not in self.collection.list_items() or overwrite:
+        if self.exists_data(name) or overwrite:
             self.collection.write(name, data, overwrite=True, metadata=kwargs)
         else:
             self.collection.append(name, data)
